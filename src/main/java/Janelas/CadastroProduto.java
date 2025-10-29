6a346c9044618040de1b678471f9145ada24e2c3
Gobato15 <gobato59@gmail.com>
2025-10-28 21:29:46 -0300

    Implementado Update dao
    Implementação do método update do produtoDAO

--- src/main/java/Janelas/CadastroProduto.java
+++ src/main/java/Janelas/CadastroProduto.java
@@ -76,6 +76,11 @@ public class CadastroProduto extends javax.swing.JFrame {
         });
 
         jBAlterar.setText("Alterar");
+        jBAlterar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBAlterarActionPerformed(evt);
+            }
+        });
 
         jBExcluir.setText("Excluir");
         jBExcluir.addActionListener(new java.awt.event.ActionListener() {
@@ -209,6 +214,20 @@ public class CadastroProduto extends javax.swing.JFrame {
         
         
     }//GEN-LAST:event_jTProdutosMouseClicked
+
+    private void jBAlterarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBAlterarActionPerformed
+      if (jTProdutos.getSelectedRow() != -1){
+        modelo.setValueAt(jTDescricao.getText(), jTProdutos.getSelectedRow(), 0);
+        modelo.setValueAt(jTQuantidade.getText(), jTProdutos.getSelectedRow(), 1);
+        modelo.setValueAt(jTValor.getText(), jTProdutos.getSelectedRow(), 2);
+        
+        Produto p = modelo.pegaDadosLinha(jTProdutos.getSelectedRow());
+        ProdutoDAO dao = new ProdutoDAO();
+        dao.update(p);
+        limpaCampos();
+        modelo.recarregaTabela();
+    }
+    }//GEN-LAST:event_jBAlterarActionPerformed
 
  
     private void limpaCampos() {
--- src/main/java/Model/ProdutoTableModel.java
+++ src/main/java/Model/ProdutoTableModel.java
@@ -47,6 +47,22 @@ public class ProdutoTableModel extends AbstractTableModel {
         return null;
     }
 
+    @Override
+    public void setValueAt(Object valor, int linha, int coluna) {
+        switch (coluna) {
+            case 0:
+                dados.get(linha).setDescricao((String) valor);
+                break;
+            case 1:
+                dados.get(linha).setQuantidade(Integer.parseInt((String) valor));
+                break;
+            case 2:
+                dados.get(linha).setValor(Double.parseDouble((String) valor));
+                break;
+        }
+        this.fireTableRowsUpdated(linha, linha);
+    }
+
     public void addLinha(Produto p) {
         this.dados.add(p);
         this.fireTableDataChanged();
@@ -68,11 +84,11 @@ public class ProdutoTableModel extends AbstractTableModel {
             this.addLinha(p);
 
         }
-        
+
         this.fireTableDataChanged();
     }
-    
-    public void recarreTabela(){
+
+    public void recarreTabela() {
         this.dados.clear();
         lerDados();
         this.fireTableDataChanged();
@@ -81,4 +97,5 @@ public class ProdutoTableModel extends AbstractTableModel {
     public void recarregaTabela() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
+
 }
